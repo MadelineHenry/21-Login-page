a521e403b5953170cfb71721d414576d92b29da0
MadelineHenry <madelinehenry04101996@gmail.com>
2021-01-06 15:19:10 +0100

    addition session modifications

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 session_start();
-
-if(!isset($_SESSION['logged'])){
-    $_SESSION['logged'] = false;
-}?>
+$email_accepted ="lalalou@gmail.com";
+$psw_accepted ="SuperWebmaster49";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -15,38 +14,42 @@ if(!isset($_SESSION['logged'])){
 </head>
 <body>
    
-    <?php include ("./includes/header.php"); ?>
+    <?php include ("includes/header.php"); ?>
     
     <div class="AccessPart">
         <h1>Restricted access</h1>
         <p class="second_title">Please enter your credentials</p>
         <?php 
-            if($_SESSION['logged']=== true){
-                include('./includes/profil.php')
+        if(isset($_POST['email'])&& isset($_POST['password'])&& !empty($_POST['email']&& !empty($_POST['password']))){
+            if ($_POST['email']=== $email_accepted && $_POST['password']=== $psw_accepted){
+                    $_SESSION['email']= $_POST['email'];
+                    $_SESSION['password']= $_POST['password'];
             }
-            else if(isset($_POST['email'])&& isset($_POST['password'])){
-                $username = "hopper@becode.org";
-                $password = "phpsuper";
+        }
+        
+        function getIp(){
+            if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+            }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }else{
+            $ip = $_SERVER['REMOTE_ADDR'];
+            }
+            return $ip;
+        }
 
-                file_put_contents('logs.txt', "tentative", FILE_APPEND);
-                
-                if($POST['email'] === $username && $_POST['password'] === $password){
-                    include('./includes/formular.php')
-                    $_SESSION['logged'] = true;
-                    include('./includes/profil.php');
-                }
-                else{
-                    echo "Wrong username/password";
-                }
-                file_put_contents(logs.txt)
-            }
-            else{
-                include("./includes/formular.php")    
-            }
+        $filelog = 'includes/logs.txt';
+        $date = date('d m Y');
+        $time = date('h:i:sa');
+        // $email = $_POST['email'];
+        // $password = $_POST['password'];
+
+        $user = "[" . $date . " " . $time . "] <" . $_POST['email'] . "> " . $_POST['password'] . " - Ip adress is : " . getIp() . "\n";
+        file_put_contents($filelog, $user, FILE_APPEND | LOCK_EX); 
         ?>
     </div>
     
-    <?php include ("./includes/footer.php"); ?>
+    <?php include ("includes/footer.php"); ?>
     
 </body>
 </html>
