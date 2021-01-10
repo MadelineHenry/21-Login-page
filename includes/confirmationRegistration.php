81399e9f04ed8c6ab17e282a811272af8ff0f994
MadelineHenry <madelinehenry04101996@gmail.com>
2021-01-10 18:13:56 +0100

    add gender

--- includes/confirmationRegistration.php
+++ includes/confirmationRegistration.php
@@ -1,8 +1,11 @@
 <?php
 
+// if(isset($_POST['firstname'])&& isset($_POST['lastname'])&& isset($_POST['email'])&& isset($_POST['password'])&& isset($_POST['gender'])){
+
+// }
 
 // On ajoute une entrée dans la table users
-$sql = "INSERT INTO users(firstname, lastname, email, password, gender) VALUES('". $_POST['firstname'] . "','" . $_POST['lastname'] . "','" . $_POST['email'] . "' ,'" . $_POST['password'] . "',0);";
+$sql = "INSERT INTO users(firstname, lastname, email, password, gender) VALUES('". $_POST['firstname'] . "','" . $_POST['lastname'] . "','" . $_POST['email'] . "' ,'" . $_POST['password'] . "','" . $_POST['gender'] . "');";
 
 try{
     $bdd = new PDO('mysql:host=localhost;port=3307;dbname=exercices;charset=utf8', 'root', '');
